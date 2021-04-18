b09029dd489bd783166a608a6c71b4405026aaa6
Sachin <sachingvit@gmail.com>
2021-04-18 17:34:49 +0530

    added header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import { ThemeContext } from './../context/themeContext'
 
+export function Logo(props) {
+    return (
+        <>
+            <img src={`${process.env.PUBLIC_URL}/logo192.png`} alt="react-todo-app" data-todo-type="header-logo" />
+        </>
+    )
+}
+
+export function Search(props) {
+    return (
+        <>
+            <input type="text" placeholder="Search" />
+            <i className="fa fa-search"></i>
+        </>
+    )
+}
+
 function Header() {
     const { theme, changeTheme } = React.useContext(ThemeContext)
+    const themeClass = theme === 'dark' ? 'fa-sun-o' : 'fa-moon-o'
     return (
         <header className="header">
-            <button onClick={changeTheme}>Switch To {theme === 'dark' ? `Light` : 'Dark'}</button>
+            <span className="logo"><Logo /></span>
+            <span className="search-section">
+                <Search />
+                <button onClick={changeTheme}>Switch To <i className={`fa ${themeClass}`}></i></button>
+
+            </span>
         </header>
     )
 }
